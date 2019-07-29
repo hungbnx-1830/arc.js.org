f5e5353af837c938ef3eb9c5668eb09d4d70945f
nguyen.xuan.hungb <nguyen.xuan.hungb@framgias-Mac-mini-48.local>
2019-07-29 09:08:54 +0700

    Update Component

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import { injectGlobal, ThemeProvider } from 'styled-components'
 
-import { HomePage } from 'components'
+import { HomePage, SamplePage, NotFoundPage } from 'components'
 
 // https://github.com/diegohaz/arc/wiki/Styling
 import theme from './themes/default'
@@ -18,6 +18,8 @@ const App = () => {
     <ThemeProvider theme={theme}>
       <Switch>
         <Route path="/" component={HomePage} exact />
+        <Route path="/sample-page" component={SamplePage} />
+        <Route component={NotFoundPage} />
       </Switch>
     </ThemeProvider>
   )
